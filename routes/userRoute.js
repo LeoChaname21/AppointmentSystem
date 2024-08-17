10c994f0fcc2638a981dbe3de376b0c249d1b7c2
LeoKill2104 <leonardo.chaname@gmail.com>
2024-08-16 21:25:42 -0500

    auth and crud completed

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const authMiddleware = require("../middlewares/authmiddleware");
+const mongoose = require("mongoose");
 
 router.post("/register", async (req, res) => {
 
@@ -32,10 +34,18 @@ router.post("/register", async (req, res) => {
 
 });
 
-router.get("/list", async (req, res) => {
+router.get("/list", authMiddleware, async (req, res) => {
     try {
         const users = await User.find({});
-        res.status(200).send({ message: "Usuarios encontrados", success: true, users });
+        res.status(200).send({
+            message: "Usuarios encontrados", success: true,
+            users: users.map(item => ({
+                id: item._id,
+                name: item.name,
+                email: item.email
+
+            }))
+        });
     } catch (error) {
         console.log(error);
         res.status(500).send({ message: "Error al obtener usuarios", success: false, error });
@@ -46,19 +56,19 @@ router.post("/login", async (req, res) => {
 
     try {
         const user = await User.findOne({ email: req.body.email });
-        if(!user){
-            return res.status(200).send({ message: "Usuario no existe", success: false});
+        if (!user) {
+            return res.status(200).send({ message: "Usuario no existe", success: false });
         }
         const isMatch = await bcrypt.compare(req.body.password, user.password);
-        if(!isMatch){
-            return res.status(200).send({ message:"La contraseña es incorrecta", success:false});
+        if (!isMatch) {
+            return res.status(200).send({ message: "La contraseña es incorrecta", success: false });
         }
-        else{
-        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET,{
-            expiresIn: "1d"
-        })
-        res.status(200).send({ message: "Ha accedido exitosamente", success: true, data:token});
-        } 
+        else {
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+                expiresIn: "1d"
+            })
+            res.status(200).send({ message: "Ha accedido exitosamente", success: true, data: token });
+        }
 
     } catch (error) {
         console.log(error);
@@ -66,5 +76,76 @@ router.post("/login", async (req, res) => {
     }
 
 });
+
+router.post("/get-user-info-by-id", authMiddleware, async (req, res) => {
+
+    try {
+        const user = await User.findOne({ _id: req.body.userId });
+        user.password = undefined;
+        if (!user) {
+            return res.status(200).send({
+                message: "Usuario no existe",
+                success: false
+            });
+        } else {
+            res.status(200).send({
+                success: true,
+                data: user
+            });
+        }
+
+    } catch (error) {
+        res.status(500).send({
+            message: "Error al cargar informacion del usuario",
+            success: false
+        });
+
+    }
+
+});
+
+router.post("/delete", async (req, res) => {
+    try {
+
+        if (!mongoose.Types.ObjectId.isValid(req.body._id)) {
+            return res.status(400).send({ message: "ID inválido", success: false });
+        }
+        const user = await User.findOne({ _id: req.body._id });
+        if (!user) {
+            return res.status(200).send({ message: "Usuario no existe", success: false });
+        } else {
+            await User.deleteOne({ _id: req.body._id });
+            res.status(200).send({ message: "Usuario Eliminado Exitosamente", success: true });
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ message: "Error al eliminar usuario", success: false, error });
+    }
+});
+
+
+router.post("/update", async (req, res) => {
+    try {
+        if (!mongoose.Types.ObjectId.isValid(req.body._id)) {
+            return res.status(400).send({ message: "ID inválido", success: false });
+        }
+        const user = await User.findOne({ _id: req.body._id });
+        if (!user) {
+            return res.status(200).send({ message: "Usuario no existe", success: false });
+        }
+        else {
+            await User.updateOne({ _id: req.body._id }, req.body);
+            res.status(200).send({ message: "Usuario Editado Exitosamente", success: true });
+
+        }
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ message: "Error al editar usuario", success: false, error });
+    }
+});
+
 
 module.exports = router;
