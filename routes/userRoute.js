46f7f360f6187562f7adcbbe8f3ff1d0f4c3eed2
LeoKill2104 <leonardo.chaname@gmail.com>
2024-08-21 10:27:16 -0500

    doctor model and notifications crud

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/userModel");
+const Doctor = require("../models/doctorModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const authMiddleware = require("../middlewares/authmiddleware");
@@ -39,12 +40,7 @@ router.get("/list", authMiddleware, async (req, res) => {
         const users = await User.find({});
         res.status(200).send({
             message: "Usuarios encontrados", success: true,
-            users: users.map(item => ({
-                id: item._id,
-                name: item.name,
-                email: item.email
-
-            }))
+            users: users
         });
     } catch (error) {
         console.log(error);
@@ -120,7 +116,7 @@ router.post("/delete", authMiddleware, async (req, res) => {
                 res.status(200).send({ message: "Usuario Eliminado Exitosamente", success: true });
             }
         }
-        else{
+        else {
             return res.status(400).send({ message: "Usuario no tiene permisos necesarios", success: false });
         }
 
@@ -131,27 +127,26 @@ router.post("/delete", authMiddleware, async (req, res) => {
 });
 
 
-router.post("/update", authMiddleware , async (req, res) => {
+router.post("/update", authMiddleware, async (req, res) => {
     try {
         const userId = await User.findOne({ _id: req.body.userId });
-        if(userId.isAdmin)
-        {
+        if (userId.isAdmin) {
 
-        if (!mongoose.Types.ObjectId.isValid(req.body._id)) {
-            return res.status(400).send({ message: "ID inválido", success: false });
-        }
-        const user = await User.findOne({ _id: req.body._id });
-        if (!user) {
-            return res.status(200).send({ message: "Usuario no existe", success: false });
-        }
-        else {
-            await User.updateOne({ _id: req.body._id }, req.body);
-            res.status(200).send({ message: "Usuario Editado Exitosamente", success: true });
+            if (!mongoose.Types.ObjectId.isValid(req.body._id)) {
+                return res.status(400).send({ message: "ID inválido", success: false });
+            }
+            const user = await User.findOne({ _id: req.body._id });
+            if (!user) {
+                return res.status(200).send({ message: "Usuario no existe", success: false });
+            }
+            else {
+                await User.updateOne({ _id: req.body._id }, req.body);
+                res.status(200).send({ message: "Usuario Editado Exitosamente", success: true });
 
+            }
+        } else {
+            return res.status(400).send({ message: "Usuario no tiene permisos necesarios", success: false })
         }
-    }else {
-        return res.status(400).send({ message: "Usuario no tiene permisos necesarios", success:false})
-    }
 
     } catch (error) {
         console.log(error);
@@ -159,5 +154,103 @@ router.post("/update", authMiddleware , async (req, res) => {
     }
 });
 
+router.post("/apply-doctor-account", authMiddleware, async (req, res) => {
+    try {
+        const newDoctor = new Doctor({ ...req.body, status: "pending" });
+        await newDoctor.save();
+        const adminUser = await User.findOne({ "isAdmin": true });
+        const unseenNotifications = adminUser.unseenNotifications;
+
+        unseenNotifications.push({
+            type: "new-doctor-request",
+            message: `${newDoctor.firstName} ${newDoctor.lastname} ha aplicado para la cuenta de Doctor`,
+            data: {
+                doctorId: newDoctor._id,
+                name: newDoctor.firstName + " " + newDoctor.lastname
+            },
+            onclickPath: "/admin/doctors"
+        })
+        await User.findByIdAndUpdate(adminUser._id, { unseenNotifications });
+
+        res.status(200).send({ message: "Ha Aplicado a Cuenta de Doctor Exitosamente", success: true });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            message: "Error al aplicar cuenta de Doctor",
+            success: false,
+            error
+        })
+
+    }
+})
+
+router.post("/mark-all-notifications-as-seen", authMiddleware, async (req, res) => {
+    try {
+        const user = await User.findOne({ _id: req.body.userId });
+        const seenNotifications = user.seenNotifications;
+        const unseenNotifications = user.unseenNotifications;
+        seenNotifications.push(...unseenNotifications);
+        user.unseenNotifications = [];
+        user.seenNotifications = seenNotifications;
+        const updateUser = await user.save();
+        updateUser.password = undefined;
+        res.status(200).send({
+            message: "Las notificaciones han sido marcadas como vistas",
+            success: true
+        });
+
+
+    } catch (error) {
+        res.status(500).send({
+            message: "Las notificaciones no fueron marcadas",
+            success: false
+        })
+
+    }
+
+});
+
+router.post("/delete-all-notifications", authMiddleware, async (req, res) => {
+    try {
+        const user = await User.findOne({ _id: req.body.userId })
+        user.seenNotifications = [];
+        user.unseenNotifications = [];
+        const updateUser = await user.save();
+        updateUser.password = undefined;
+        res.status(200).send({
+            message: "Todas las notificaciones han sido eliminadas",
+            success: true
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(200).send({
+            message: "Error al eliminar las notificaciones",
+            success: false,
+            error
+        });
+    }
+
+})
+
 
 module.exports = router;
+
+
+// {
+//     "userId":"ss",
+//     "firstName":"ss",
+//     "lastname":"ss",
+//     "email":"ss",
+//     "phoneNumber":15,
+//     "website":"ss",
+//     "address":"ss",
+//     "specialization":"ss",
+//     "experience":"ss",
+//     "feePerConsultation": 15,
+//     "timings":{
+//         "name": "leo",
+//         "name2": "sex"
+//     }
+//     }
